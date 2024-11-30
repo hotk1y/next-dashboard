b233e17ac05b14a1e5914e89752f670a859b744d
hotk3y <92748607+hotk1y@users.noreply.github.com>
2024-11-30 22:59:01 +0500

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -28,9 +28,9 @@ export default function Page() {
                </Link>
             </div>
             <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-               {/* <div
+               <div
                   className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
-               /> */}
+               />
                {/* Add Hero Images Here */}
                <Image
                   src="/hero-desktop.png"
